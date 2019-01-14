9b87fca820108adaa48016706beb5b64c0950927
riccjohn <riccjohn@gmail.com>
2019-01-14 12:00:47 -0600

    Added link to external page for each book

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -5,11 +5,15 @@ const Book = props => {
 
   return (
     <div className='bookResult'>
-      <h3>{book.volumeInfo.title}</h3>
+      <h3><a href={book.volumeInfo.previewLink}>{book.volumeInfo.title}</a></h3>
       <p>by {book.volumeInfo.authors.join(', ')}</p>
       {book.volumeInfo.imageLinks ? (
         <img src={book.volumeInfo.imageLinks.thumbnail} alt='thumbnail' />
-      ) : <p>Image not available</p>}      
+      ) : <p>Image not available</p>}
+      {book.volumeInfo.publisher && (
+        <p>Published by {book.volumeInfo.publisher}</p>
+      )}
+
     </div>
   )
 }
